be1d9ebda6818a1c22527facf4f08ed9bffc5651
Jacob Christensen Admin <jChristensenAdmin@weimerbearing.com>
2024-10-04 16:46:21 -0500

    Add first two solutions

---
+++ AdditionCalculator/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace AdditionCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Prompt the user to enter the first number
+            Console.WriteLine("Enter the first number: ");
+
+            // Get User input to store the first number
+            string? userInput = Console.ReadLine();
+
+            Console.WriteLine("You entered: " + userInput);
+
+
+        }
+    }
+}
---
+++ HelloWorld/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            // Declare and assign a (string) variable
+            string myName = "Jacob";
+            // Modify the value of a string variable
+            myName = "Jacob Christensen";
+            // Use/access the variable to output the value to the console
+            Console.WriteLine(myName);
+
+        }
+    }
+}
